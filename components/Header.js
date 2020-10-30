777d70ea87c7f6820dafdb9b39039a0ad14d4b91
sebastian espeset <sebastian.c.espeset@gmail.com>
2020-10-30 10:23:04 -0700

    working on header

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,15 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    //This function will create elements, establish a hierarchy by appending them appropriately, and establish content within them..
+    
+    //creating the elements;
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temperature = document.createElement('span');
+
+    //Hierarchy;
+    
+}
